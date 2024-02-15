040a2c0234417f1a5857964684f6cfd496703a6a
Jeremy Edwards <1312331+jeremyje@users.noreply.github.com>
2024-02-15 05:17:14 +0000

    Add tests for clean_empty_directory

--- src/clean_empty_directory/mod.rs
+++ src/clean_empty_directory/mod.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use indicatif::ProgressBar;
 use log::warn;
 
 #[derive(clap::Args)]
@@ -25,57 +25,46 @@ pub(crate) struct Args {
     pub(crate) dry_run: std::primitive::bool,
 }
 
-fn new_progress_bar(multi_progress: &MultiProgress, spinner_style: &ProgressStyle) -> ProgressBar {
-    let progress_bar = multi_progress.add(ProgressBar::new(1));
-    progress_bar.set_style(spinner_style.clone());
-    progress_bar
-}
-
 pub(crate) fn run(args: &Args) -> std::io::Result<()> {
-    let spinner_style = ProgressStyle::with_template("{prefix:.bold.dim} {spinner} {wide_msg}")
-        .unwrap()
-        .tick_chars("⠁⠂⠄⡀⢀⠠⠐⠈ ");
-    let multi_progress = MultiProgress::new();
+    let progress_factory = crate::common::progress::ProgressFactory::new();
     let mut threads = Vec::new();
+    let pb_title = progress_factory.create_title();
+    let pb_detail = progress_factory.create_detail();
+
+    pb_title.set_prefix("Clean Empty Directories");
+    pb_title.set_message("Scanning...");
     for path in crate::common::fs::canonical_paths(args.path.as_slice())? {
         let dry_run = args.dry_run;
         let path_str = path.to_str().map_or(String::from("unknown"), String::from);
-        let progress_bar = new_progress_bar(&multi_progress, &spinner_style);
-        let action_progress_bar = new_progress_bar(&multi_progress, &spinner_style);
 
+        let pb_detail = pb_detail.clone();
         let handle = std::thread::Builder::new()
             .name(format!("clean-{path_str}"))
-            .spawn(move || {
-                match clean_empty_directory_with_progress(
-                    &progress_bar,
-                    &action_progress_bar,
-                    &path,
-                    dry_run,
-                ) {
+            .spawn(
+                move || match clean_empty_directory(&pb_detail, &path, dry_run) {
                     Ok(_) => {}
                     Err(error) => {
                         warn!("got error {error}");
                     }
-                }
-                progress_bar.finish_with_message("done");
-                action_progress_bar.finish_with_message("done");
-            })?;
+                },
+            )?;
         threads.push(handle);
     }
     for t in threads {
         t.join().expect("thread failed to join");
     }
+    pb_detail.finish_and_clear();
+    pb_title.finish_with_message("Done");
     Ok(())
 }
 
-fn clean_empty_directory_with_progress(
-    progress_bar: &ProgressBar,
-    action_progress_bar: &ProgressBar,
+fn clean_empty_directory(
+    pb_detail: &ProgressBar,
     dir_path: &std::path::Path,
     dry_run: bool,
 ) -> std::io::Result<bool> {
-    progress_bar.set_message(format!("{dir_path:#?}"));
-    progress_bar.inc(1);
+    pb_detail.set_message(format!("{dir_path:#?}"));
+    pb_detail.inc_length(1);
 
     let mut has_item = false;
     if dir_path.is_dir() {
@@ -85,12 +74,7 @@ fn clean_empty_directory_with_progress(
                 has_item = true;
             } else if let Ok(metadata) = dir_entry.symlink_metadata() {
                 if metadata.is_dir() && !metadata.is_symlink() {
-                    has_item |= !clean_empty_directory_with_progress(
-                        progress_bar,
-                        action_progress_bar,
-                        &dir_entry,
-                        dry_run,
-                    )?;
+                    has_item |= !clean_empty_directory(pb_detail, &dir_entry, dry_run)?;
                 } else {
                     has_item = true;
                 }
@@ -102,9 +86,71 @@ fn clean_empty_directory_with_progress(
     let can_delete = !has_item;
     if can_delete {
         let dry_run_text = if dry_run { "DRY RUN" } else { "" };
-        action_progress_bar.set_message(format!("{dry_run_text} {dir_path:#?}"));
-        action_progress_bar.inc(1);
+        pb_detail.set_message(format!("{dry_run_text} {dir_path:#?}"));
+        pb_detail.inc(1);
         crate::common::fs::delete_directory(dir_path, dry_run)?;
     }
     Ok(can_delete)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+    use std::path::Path;
+    use std::path::PathBuf;
+    use tempdir::TempDir;
+
+    #[test]
+    fn clean_empty_directory_remove_all() {
+        let tmp_dir = TempDir::new("clean_dir").expect("create directory");
+        fs::create_dir_all(Path::join(tmp_dir.path(), "1/2/3/4/5/6")).expect("create directory");
+        fs::create_dir_all(Path::join(tmp_dir.path(), "2/3/4/5/6")).expect("create directory");
+        fs::create_dir_all(Path::join(tmp_dir.path(), "3/4/5/6")).expect("create directory");
+        fs::create_dir_all(Path::join(tmp_dir.path(), "4/5/6")).expect("create directory");
+        fs::create_dir_all(Path::join(tmp_dir.path(), "5/6")).expect("create directory");
+        fs::create_dir_all(Path::join(tmp_dir.path(), "6")).expect("create directory");
+        run(&Args {
+            path: vec![PathBuf::from(tmp_dir.path())],
+            dry_run: true,
+        })
+        .expect("clean_empty_directories");
+        assert!(Path::join(tmp_dir.path(), "1/2/3/4/5/6").exists());
+
+        run(&Args {
+            path: vec![PathBuf::from(tmp_dir.path())],
+            dry_run: false,
+        })
+        .expect("clean_empty_directories");
+        assert_eq!(tmp_dir.path().exists(), false);
+    }
+
+    #[test]
+    fn clean_empty_directory_remove_some() {
+        let tmp_dir = TempDir::new("clean_dir").expect("create directory");
+        fs::create_dir_all(Path::join(tmp_dir.path(), "1/2/3/4/5/6")).expect("create directory");
+        fs::create_dir_all(Path::join(tmp_dir.path(), "2/3/4/5/6")).expect("create directory");
+        fs::write(Path::join(tmp_dir.path(), "1/2/3/4.txt"), b"4").expect("write file");
+        run(&Args {
+            path: vec![PathBuf::from(tmp_dir.path())],
+            dry_run: true,
+        })
+        .expect("clean_empty_directories");
+        assert!(tmp_dir.path().exists());
+        assert!(Path::join(tmp_dir.path(), "1/2/3/4/5/6").exists());
+        assert!(Path::join(tmp_dir.path(), "2").exists());
+        assert!(Path::join(tmp_dir.path(), "1/2/3/4.txt").exists());
+
+        run(&Args {
+            path: vec![PathBuf::from(tmp_dir.path())],
+            dry_run: false,
+        })
+        .expect("clean_empty_directories");
+
+        assert!(tmp_dir.path().exists());
+        assert_eq!(Path::join(tmp_dir.path(), "1/2/3/4").exists(), false);
+        assert_eq!(Path::join(tmp_dir.path(), "2").exists(), false);
+        assert!(Path::join(tmp_dir.path(), "1/2/3").exists());
+        assert!(Path::join(tmp_dir.path(), "1/2/3/4.txt").exists());
+    }
 }
